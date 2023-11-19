2b912a4d0226c2a1d29c9b3cb60a1756a406eea9
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2023-11-18 20:45:45 -0800

    nov18.1

--- src/WorldNews.jsx
+++ src/WorldNews.jsx
@@ -1,46 +1,96 @@
 import './index.css'
-import { useMemo } from "react"
-import { GoogleMap, useLoadScript, Marker} from '@react-google-maps/api';
 
 
-function Map(){
+export default function Map(){
 
-const center = useMemo(() => ({lat: 48.21, lng: -122.68}), [])
+
    
 return  (
     <div>
         <div style={{
             width: 800,
-            height: 200,
+            height: 'max-content',
             backgroundColor: 'beige',
             margin: 10,
+            padding: 10,
             fontFamily: 'monospace',
             color: '#242424'
         }}> 
         <h1 style={{marginLeft: 20}}>From the Roof of the World</h1>
-        <p style={{marginLeft: 20}}>Stories from Mazatlan</p>
-        <li style={{marginLeft: 50}}><a href='https://wwww.amazon.com'>Mudding Roof Morning</a></li>
-        <li style={{marginLeft: 50}}>Mexico May Never Leak Again</li>
+        <p style={{marginLeft: 20}}>story one</p>
         
-        <p style={{marginLeft: 20}}>Roofing over the Pandemic</p>
-        <p style={{marginLeft: 20}}>Wuhan Lab, believe it or not, needs a roof</p>
+        
+        <h1 style={{marginLeft: 20}}>Roofing over the Pandemic</h1>
+        <p style={{marginLeft: 20}}>
+            Lo Wuhan Lab, believe it or not, needs a roof. 
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            <h3>"What the hell is that?" they asked.</h3>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolore dolor, dolorum et modi natus non repellat placeat 
+            possimus porro quasi sit sed cum culpa aperiam eius ea 
+            voluptas expedita itaque!
+            </p>
         </div>
        
 
-    <GoogleMap zoom={10} center={center} mapContainerClassName='map'>
+        <div style={{
+            width: 800,
+            height: 100,
+            backgroundColor: '#2423',
+            margin: 10,
+            padding: 10,
+            fontFamily: 'monospace',
+            color: 'orange',
 
-    <Marker position={center}/>
-    
-    </GoogleMap>
+        }}>
+           <h3>footer</h3>
+           <p>information of the footer sort</p>
+        </div>
     </div>
    
     )
 }
 
-export default function WorldNews(){
-
-    const { isLoaded } = useLoadScript({googleMapsApiKey: 'AIzaSyD05eVSura_VEKmhs-KvGBxZlU9Yftht2c'})
-
-    if ( isLoaded ) return <div><Map/></div>
-    
-}
