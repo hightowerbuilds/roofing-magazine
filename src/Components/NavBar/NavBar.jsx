0bee203fa949afcf10b069dbe1028a60faba6953
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2025-05-10 10:58:35 -0700

    working on home page

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,10 +6,11 @@ export default function NavBar() {
     <div className='navBarMainContainer'>
 
         <NavLink to='/'><button className='navButton'>home</button></NavLink>
-        <NavLink to='/blog'> <button className='navButton'>writing</button></NavLink>
-        <NavLink to='/art'> <button className='navButton'>visions</button></NavLink>
-        <NavLink to='/games'> <button className='navButton'>games</button></NavLink>
+        <NavLink to='/blog'><button className='navButton'>writing</button></NavLink>
+        <NavLink to='/art'><button className='navButton'>visions</button></NavLink>
+        <NavLink to='/games'><button className='navButton'>games</button></NavLink>
 
     </div>
   )
 }
+4
