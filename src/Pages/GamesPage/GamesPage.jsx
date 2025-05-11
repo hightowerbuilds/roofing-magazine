965544585ebdfd0fe42080c00a3b5f3e59298939
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2025-05-11 11:07:01 -0700

    more routing issues

--- src/Pages/GamesPage/GamesPage.jsx
+++ src/Pages/GamesPage/GamesPage.jsx
@@ -1,5 +1,4 @@
 
-import { NavLink } from 'react-router-dom'
 import NavBar from '../../Components/NavBar/NavBar'
 
 export default function GamesPage() {
@@ -7,12 +6,9 @@ export default function GamesPage() {
     <div>
 
         <NavBar />
-        
         games page
 
-        bad jump
-        <NavLink to='/badjump'><button className='navButton'>bad jump</button></NavLink>
-   
+      
     </div>
   )
 }
