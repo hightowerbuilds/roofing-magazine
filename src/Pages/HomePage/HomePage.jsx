d309f3b510abe01305e5cf66869a6da97f6d26be
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2025-05-11 08:26:36 -0700

    getting more images involved

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -11,14 +11,25 @@ import { supabase } from '../../services/supabase';
 export default function HomePage() {
 
     const [images, setImages] = useState();
+    const [backgroundImage, setBackgroundImage] = useState();
     const [loading, setLoading] = useState(true);
 
  
 
     useEffect(() => {
 
+        async function loadBackgroundImage(){
+            const { data, error } = await supabase.storage.from('images').getPublicUrl('backgrounds/texture-background.png');
+            if (error) {
+                console.error('Error fetching images:', error);
+            } else {
+                setImages(data.publicUrl);
+            }
+            setLoading(false);
+        }
+
         async function loadImages() {
-            const { data, error } = await supabase.storage.from('images').getPublicUrl('titles')
+            const { data, error } = await supabase.storage.from('images').getPublicUrl('titles/roofing-magazine.png');
             if (error) {
                 console.error('Error fetching images:', error);
             } else {
@@ -28,14 +39,29 @@ export default function HomePage() {
         }
 
         loadImages();
+        loadBackgroundImage();
     }, []);
 
   return (
-    <div className='homePageMainContainer'> 
+    <div className='homePageMainContainer' style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '100vh',
+    }}> 
         <NavBar />
 
-       <div className='homePageHeading'>
-        <img src={images} />
+       <div 
+        className='homePageHeading' 
+        style={{ 
+            backgroundImage: `url(${images})`, 
+            width: '25%', 
+            height: '15vh', 
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center' 
+            }}>
+
         </div>
 
         <div className='homePageSectionOne'>
